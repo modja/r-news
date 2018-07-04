a19d3ca7987ea609963e0947b4240b30c44d3830
root <sandyharyono@gmail.com>
2018-07-04 21:46:14 +0700

    single page exclude related post

--- wp-content/themes/r-news/single.php
+++ wp-content/themes/r-news/single.php
@@ -14,16 +14,15 @@ Single.php
 -->
 
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main">
-
+	<article class="single-post">
+    		<div class="container">
 		<?php
 		while ( have_posts() ) :
 			the_post();
 
 			get_template_part( 'template-parts/content', get_post_type() );
 
-			the_post_navigation();
+			//the_post_navigation();
 
 			// If comments are open or we have at least one comment, load up the comment template.
 			if ( comments_open() || get_comments_number() ) :
@@ -33,8 +32,8 @@ Single.php
 		endwhile; // End of the loop.
 		?>
 
-		</main><!-- #main -->
-	</div><!-- #primary -->
+		</div><!-- #div -->
+	</article><!-- #primary -->
 
 <?php
 //get_sidebar();
