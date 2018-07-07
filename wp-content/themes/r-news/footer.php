1feae90ce59310949a2073bb51138d4f9343aead
root <sandyharyono@gmail.com>
2018-07-08 00:17:03 +0700

    subscribe done and added plugin for mailchimp

--- wp-content/themes/r-news/footer.php
+++ wp-content/themes/r-news/footer.php
@@ -15,7 +15,7 @@
 <?php wp_footer(); ?>
 
 
-
+  
 
 
  <!-- NEWSLETTER -->
@@ -24,22 +24,24 @@
       <div class="container is-fullhd">
         <div class="columns is-mobile">
 
-          <div class="column has-text-centered inner">
-            <h4 class="title is-4 is-uppercase">Subsribe Newsletter Kami</h4>
-            <p>Langganan untuk update semua berita dan informasi kami Kami</p>
-            <br>
-            <div class="columns is-flex is-centered is-gapless">
-              <div class="field has-addons">
-                <div class="control">
-                  <input class="input is-medium is-rounded" type="text" placeholder="Alamat Email Anda">
-                </div>
-                <div class="control">
-                  <a class="button is-medium is-rounded is-warning">Berlangganan</a>
-                </div>
-              </div>
-            </div>
-          </div>
-
+			  <div class="column has-text-centered inner">
+			    <h4 class="title is-4 is-uppercase">Subsribe Newsletter Kami</h4>
+			    <p>Langganan untuk update semua berita dan informasi kami Kami</p>
+			    <br>
+			    <div class="columns is-flex is-centered is-gapless">
+			      <div class="field has-addons">
+				
+ 					<div class="control">
+					  <input class="input is-medium is-rounded" type="text" name="email" id="email" placeholder="Alamat Email Anda">
+					</div>
+					<div class="control">
+					  <a class="button is-medium is-rounded is-warning" id="formSubscribeSubmitBtn">Berlangganan</a>
+					</div>
+ 			      
+				</div>
+			    </div>
+			  </div>
+		
         </div>
       </div>
     </section>
@@ -85,7 +87,7 @@
       });
 
 
-
+	
 
       // Modals
       $(".modal-button").click(function(e) {
@@ -100,6 +102,48 @@
          $(this).parent().parent().parent().removeClass("is-active");
       });
 
+
+     //subscribe
+      $("#formSubscribeSubmitBtn").click(function(event){
+	event.preventDefault();
+	
+	var email = $( "#email" );
+	if(email.val()== '')	{
+		alert('Email is mandatory!');
+		return;	
+	}
+
+	if(!isValidEmailAddress(email.val()))	{
+		alert('Invalid email format');
+		return;	
+	}
+
+	     		
+	$.ajax({
+	    //get this url after we registered at mailtarget.co 
+	    url: "https://pt-provate-5ws.mailtrgt.com/form/5b40e6f76cb62a1b4190da33",
+	    beforeSend: function(xhr) {},
+	    type: 'POST',
+	    dataType: 'json',
+	    contentType: 'application/json',
+	    processData: false,
+	    data: '{"email":"' + email.val() +'"}',
+	    success: function (data) {
+	      	alert('Subscribe Success');
+		email.val('');
+	    },
+	    error: function(){
+	      alert("Cannot get data");
+	    }
+  	 });
+  	 
+      });
+
+
+   function isValidEmailAddress(emailAddress) {
+    var pattern = /^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([ \t]*\r\n)?[ \t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([ \t]*\r\n)?[ \t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i;
+    return pattern.test(emailAddress);
+   }
 
 
 
