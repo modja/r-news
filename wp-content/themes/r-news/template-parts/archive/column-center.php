311204e91b999eeb802c5bf56ee4de4f8121e75e
modja <mochdja@gmail.com>
2018-07-09 10:02:23 +0700

    add link to archive post title

--- wp-content/themes/r-news/template-parts/archive/column-center.php
+++ wp-content/themes/r-news/template-parts/archive/column-center.php
@@ -33,8 +33,8 @@ global $post,$cat;
                   <span><?php echo get_cat_name($cat);?></span>
                 </div>
 	           
-                <!--<h4 class="title"><a href="<?php echo esc_url( get_permalink() ) ?>"> <?php the_title() ?></a></h4>-->
-                <h4 class="title"><?php the_title() ?></h4>
+                <h4 class="title"><a href="<?php echo esc_url( get_permalink() ) ?>"> <?php the_title() ?></a></h4>
+                <!-- <h4 class="title"><?php the_title() ?></h4> -->
 
                 <div class="post-meta columns is-mobile is-gapless">
                   <div class="column has-text-left">
@@ -81,7 +81,7 @@ global $post,$cat;
                   <div class="post-categories">
                     <span>Entrepeuneurs</span>
                   </div>
-                  <h4 class="title">Business Gathering Bersama Ralali.Com “Drive Businesses In E-Commerce</h4>
+                  <h4 class="title"><a href="#">Business Gathering Bersama Ralali.Com “Drive Businesses In E-Commerce</a></h4>
                   <div class="post-meta columns is-mobile is-gapless">
                     <div class="column has-text-left">
                       <span class="post-date">2 jam lalu</span>
